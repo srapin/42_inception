9688592eb4bd0ae631ac28e860abc9484548996e
vboxuser <vboxuser@youhou.myguest.virtualbox.org>
2024-10-05 23:03:01 +0200

    mariadb_init

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -1,25 +1,31 @@
+#!/bin/bash
+
 apt_yes_exec(){
     apt $* -y
 }
 
+apt_up(){
+    for cmd in "update" "upgrade"; do
+        apt_yes_exec $cmd
+    done
+}
+
 apt_install(){
     for prog in $*; do
-        apt_yes_exec "intall" $prog
+        apt_yes_exec "install" $prog
     done;
 }
 
 apt_mariadb(){
-    for cmd in "update" "upgrade"; do
-        apt_yes_exec $cmd
-    done
-    apt_install "mariab-server" "vim" "curl" "OpenSSL"
+    apt_up
+    apt_install "mariadb-server" #"vim" "curl" "OpenSSL"
 }
 
-dir_creation(){
-    for dir in "/etc/nginx/ssl" "/var/run/nginx"; do
-        mkdir -p $dir
-    done
-}
+# dir_creation(){
+#     for dir in "/etc/nginx/ssl" "/var/run/nginx"; do
+#         mkdir -p $dir
+#     done
+# }
 
-dir_creation
-apt_nginx
+# dir_creation
+apt_mariadb
